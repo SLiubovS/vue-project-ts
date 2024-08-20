1ce4a5b6a606ec1c2ceeb20b800171d05aac19b0
Люба <gareevaliubov@gmail.com>
2024-08-20 15:22:50 +0300

    скорректировала app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,44 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <nav class="router">
+    <RouterLink to="/" class="router_margin">Users</RouterLink>
+    <RouterLink to="/AddUserView" class="router_margin">AddUser</RouterLink>
+  </nav>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="container">
+    <RouterView/>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+.router {
+  display: flex;
+  flex-direction: row;
+  position: absolute;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  height: 100%;
+  justify-content: center;
+  align-items: start;
+  margin-top: 50px;
+  font-size: 25px;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.router_margin {
+  margin: 20px;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.container {
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: start;
+  margin-top: 120px;
 }
 </style>
---
+++ src/views/AddUserView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>ты на доп. странице</h1>
+  </div>
+</template>
+
+<style>
+
+</style>

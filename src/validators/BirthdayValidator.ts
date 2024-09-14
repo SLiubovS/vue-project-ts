1e7c289c73d7107d25609bbe74bf0d42ae925a6e
Люба <gareevaliubov@gmail.com>
2024-09-14 15:51:21 +0300

    добавила валидацию даты

---
+++ src/validators/BirthdayValidator.ts
@@ -0,0 +1,25 @@
+import { FieldValidator } from "./FieldValidator";
+import { ValidationResult } from "./ValidationResult";
+
+export class BirthdayValidator extends FieldValidator {
+
+    constructor(fieldName: string, fieldValue: string | null) {
+        super(fieldName, fieldValue);
+    }
+
+    validate(validationResults: Array<ValidationResult>): boolean {
+
+        if (this._fieldValue == "" || this._fieldValue == null) {
+            return true;
+        }
+
+        const today = new Date();
+
+        if (new Date(this._fieldValue) > today) {
+            validationResults.push(new ValidationResult(this._fieldName, "Дата заполнена не корректно"));
+            return false;
+        }
+
+        return true;
+    }
+}

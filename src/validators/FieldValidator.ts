e1c06f9fc2a2d1422ce1a39b9a7b628525a00cc6
Люба <gareevaliubov@gmail.com>
2024-09-14 15:37:11 +0300

    создала абстрактный класс FieldValidator

---
+++ src/validators/FieldValidator.ts
@@ -0,0 +1,24 @@
+import type { IValidator } from "./IValidator";
+import type { ValidationResult } from "./ValidationResult";
+
+export abstract class FieldValidator implements IValidator {
+
+    private _fieldName: string;
+    private _fieldValue: string | null;
+
+    constructor(fieldName: string, fieldValue: string | null) {
+        this._fieldName = fieldName;
+        this._fieldValue = fieldValue;
+    }
+
+    public get fieldName() {
+        return this._fieldName;
+    }
+
+    public get fieldValue() {
+        return this._fieldValue;
+    }
+
+    abstract validate(validationResults: Array<ValidationResult>): boolean;
+
+}

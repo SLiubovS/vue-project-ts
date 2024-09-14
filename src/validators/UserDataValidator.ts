25e3cbf4821eaff759b303db7d159f20b170f494
Люба <gareevaliubov@gmail.com>
2024-09-14 16:45:29 +0300

    работа с валидацией

--- src/validators/UserDataValidator.ts
+++ src/validators/UserDataValidator.ts
@@ -1,6 +1,9 @@
 import type { IValidator } from "./IValidator";
 import type { IUserData } from "../models/IUserData";
 import type { ValidationResult } from "./ValidationResult";
+import { EmptyValidator } from "./EmptyValidator";
+import { NameValidator } from "./NameValidator";
+import { BirthdayValidator } from "./BirthdayValidator";
 
 export class UserDataValidator implements IValidator {
 
@@ -15,7 +18,35 @@ export class UserDataValidator implements IValidator {
     }
 
     validate(validationResults: Array<ValidationResult>): boolean {
-        throw new Error("Method not implemented.");
+
+        const validators: Array<IValidator> = [
+            new EmptyValidator("lastName", this._userData.lastName),
+            new EmptyValidator("firstName", this._userData.firstName),
+            new EmptyValidator("birthday", this._userData.birthday),
+            new NameValidator("lastName", this._userData.lastName),
+            new NameValidator("firstName", this._userData.firstName),
+            new BirthdayValidator("birthday", this._userData.birthday)
+        ];
+
+        let isValid = true;
+
+        for (let validator of validators) {
+
+            let isValidField = validator.validate(validationResults);
+
+            if (!isValidField) {
+                isValid = false;
+            }
+        }
+
+        // провалидировать что ластнейм заполнено
+        // провалидировать что фестнейм заполнено
+        // провалидировать что дата заполнена
+        // провалидировать что ластнейм заполнено корректно (русские символы)
+        // провалидировать что фестнейм заполнено корректно (русские символы)
+        // провалидировать что дата заполнено корректно (в виде даты, не большей чем сегодня)
+
+        return isValid;
     }
     
 }

baae684de03cbc668188bc1fbe6f5e2e53a86403
Люба <gareevaliubov@gmail.com>
2024-09-29 13:55:57 +0300

    убрала Ref из хранилища users

--- src/storages/UseUsersStore.ts
+++ src/storages/UseUsersStore.ts
@@ -6,17 +6,17 @@ import type { IUserEdit } from '../models/IUserEdit';
 
 export const useUsersStore = defineStore('users-store', () => {
 
-    const users = ref<User[]>([
+    const users = [
         new User(5, "Александр", "Соловьев", "Анатольевич", "1961-06-27T00:00:00.000+03:00"),
         new User(2, "Любовь", "Соловьева", "Салаватовна", "1994-04-13T00:00:00.000+03:00"),
         new User(3, "Кисяо", "Соловьева", null, "2019-08-19T00:00:00.000+03:00"),
         new User(4, "Майя", "Гареева", "Эдуардовна", "1966-04-30T00:00:00.000+03:00"),
         new User(1, "Лариса", "Соловьева", "Николаевна", "1968-04-12T00:00:00.000+03:00"),
-    ])
+    ];
 
     function getMaxId(): number {
         let maxId = -1;
-        for (let user of users.value) {
+        for (let user of users) {
             if (user.id > maxId) {
                 maxId = user.id;
             }
@@ -27,12 +27,12 @@ export const useUsersStore = defineStore('users-store', () => {
     function create(user:IUserAdd): void {
         let id = getMaxId() + 1;
         const newUser = new User(id, user.firstName, user.lastName, user.surName, user.birthday);
-        users.value.push(newUser);        
+        users.push(newUser);        
     }
 
     function update(userUpdate:IUserEdit): void {
 
-        const findedUser = users.value.find(user => user.id == userUpdate.id);
+        const findedUser = users.find(user => user.id == userUpdate.id);
 
         if (findedUser == null) {
             throw Error("Пользователь не найден");
@@ -43,8 +43,8 @@ export const useUsersStore = defineStore('users-store', () => {
 
     function remove(id: number): void {
 
-        const userDelete = users.value.findIndex(user => user.id == id);
-        users.value.splice(userDelete, 1);
+        const userDelete = users.findIndex(user => user.id == id);
+        users.splice(userDelete, 1);
     }
 
     return { users, create, update, remove }

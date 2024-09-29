f99206fbf5780ebab9f7253d23eeab3879465477
Люба <gareevaliubov@gmail.com>
2024-09-29 16:31:03 +0300

    нормализовала работу с поиском

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 
 import { ref } from "vue";
+import type { Ref } from "vue";
 import { useUsersStore } from "../storages/UseUsersStore";
 import { useRouter } from "vue-router";
 import _ from "lodash";
+import moment from "moment";
 
 const router = useRouter();
 const usersStore = useUsersStore();
@@ -18,9 +20,55 @@ const sortOrder: { [key: string]: boolean } = {
   birthday: true
 };
 
+const filterInput: Ref<{ [key: string]: string}> = ref({
+  id: "",
+  age: "",
+  lastName: "",
+  firstName: "",
+  surName: "",
+  birthday: ""
+});
+
 function sortBy(field: string): void {
   users.value = _.orderBy(users.value, field, sortOrder[field] ? "asc" : "desc");
   sortOrder[field] = !sortOrder[field];
+}
+
+function searchByNumber(field: "id" | "age") : void {
+
+  const value = parseInt(filterInput.value[field]);
+
+  if (!Number.isNaN(value)) {
+    users.value = usersStore.users.filter(user => user[field] == value);
+  }
+  else {
+    users.value = usersStore.users;
+  }
+}
+
+function searchByName(field: "lastName" | "firstName" | "surName" ) : void {
+
+  const value = filterInput.value[field];
+
+  if (value.length > 0) {
+    users.value = usersStore.users.filter(user =>  user[field]?.toLowerCase().includes(value.toLowerCase())); 
+  } else {
+    users.value = usersStore.users; 
+  }
+}
+
+function searchByDate(field: "birthday"): void {
+
+  const value = filterInput.value[field];
+
+  if (value.length > 0) {
+    const inputDate = moment(value).toISOString(true).split("T")[0];
+    users.value = usersStore.users.filter(user => moment(user.birthday).toISOString(true).split("T")[0] == inputDate);
+  }
+  else {
+    filterInput.value[field] = "0000-00-00";
+    users.value = usersStore.users;
+  }
 }
 
 function goToEdit(id: number): void {
@@ -31,117 +79,6 @@ function userDelete(id: number): void {
   usersStore.remove(id);
 };
 
-const inputId = ref<string>("");
-const inputAge = ref<string>("");
-const inputLastName = ref<string>("");
-const inputFirstName = ref<string>("");
-const inputSurName = ref<string>("");
-const inputBirthday = ref<string>("");
-
-const regexp = /^[А-яЁё]+$/gi;
-
-function searchByInputId() {
-
-  let idNumber = parseInt(inputId.value);
-
-  if (!Number.isNaN(idNumber)) {
-    users.value = usersStore.users.filter(user => user.id == idNumber);
-  }
-  else {
-    users.value = usersStore.users; 
-  }
-}
-
-function searchByInputLastName() {
-
-  if (inputLastName.value.match(regexp) != null) {
-    if (inputLastName.value != null) {
-    users.value = usersStore.users.filter(user => user.lastName.toLowerCase().includes(inputLastName.value.toLowerCase()));
-  }
-  else {
-    users.value = usersStore.users;
-  }
-}
-else {
-  if (inputLastName.value == "") {
-    users.value = usersStore.users;
-  }
-  else {
-  alert( Error("Фамилия должна содержать только русские буквы"));
-  inputLastName.value = "";
-  }
-}
-  }
-
-function searchByInputFirstName() {
-
-  if (inputFirstName.value.match(regexp) != null) {
-  if (inputFirstName.value != null) {
-    users.value = usersStore.users.filter(user => user.firstName.toLowerCase().includes(inputFirstName.value.toLowerCase()));
-  }
-  else {
-    users.value = usersStore.users;
-  }
-}
-else {
-  if (inputFirstName.value == "") {
-    users.value = usersStore.users;
-  }
-  else {
-    alert( Error("Имя должно содержать только русские буквы"));
-    inputFirstName.value = "";
-  }
-}
-}
-
-function searchByInputSurName() {
-
-  if (inputSurName.value.match(regexp) != null) {
-  if (inputSurName.value != null) {
-    users.value = usersStore.users.filter(user => user.surName.toLowerCase().includes(inputSurName.value.toLowerCase()));
-  }
-  else {
-    users.value = usersStore.users;
-  }
-} 
-else {
-  if (inputSurName.value == "") {
-    users.value = usersStore.users;
-  }
-  else {
-  alert( Error("Отчество должно содержать только русские буквы"));
-  inputSurName.value = "";
-  }
-}
-}
-
-const color = ref(true);
-function searchByInputBirthday() {
-
-  if (inputBirthday.value.length > 0) {
-    color.value = false;
-    const inpBirthday = new Date(inputBirthday.value).toISOString().split("T")[0];
-    users.value = usersStore.users.filter(user => user.birthday.toISOString().split("T")[0] == inpBirthday);
-  }
-
-  else {
-    inputBirthday.value = "0000-00-00";
-    users.value = usersStore.users;
-    color.value = true;
-  }
-}
-
-function searchByInputAge() {
-  
-  let ageNumber = parseInt(inputAge.value);
-
-  if (!Number.isNaN(ageNumber)) {
-    users.value = usersStore.users.filter(user => user.age == ageNumber);
-  }
-  else {
-    users.value = usersStore.users;
-  }
-}
 </script>
 
 <template>
@@ -153,44 +90,43 @@ function searchByInputAge() {
           <th scope="col-1" class="table__input_size">
             <div @click="sortBy('id')" class="table__div_padding table_align">ID</div>
             <div>
-              <input class="form-control form-control" type="number" v-model="inputId" @input="searchByInputId" placeholder="поиск по id">
+              <input class="form-control form-control" type="number" v-model="filterInput.id" @input="searchByNumber('id')" placeholder="поиск по id">
             </div>
           </th>
           <th scope="col-2">
             <div @click="sortBy('lastName')" class="table__div_padding table_align">Фамилия</div>
             <div>
-              <input class="form-control" type="text" size="10" oninput="this.size=Math.max(this.value.length, 10)" v-model="inputLastName" placeholder="по фамилии"
-                @input="searchByInputLastName">
+              <input class="form-control" type="text" size="10" oninput="this.size=Math.max(this.value.length, 10)" v-model="filterInput.lastName" placeholder="по фамилии"
+                @input="searchByName('lastName')">
             </div>
           </th>
           <th scope="col-2">
             <div @click="sortBy('firstName')" class="table__div_padding table_align">Имя</div>
             <div>
-              <input class="form-control" type="text" size="10" oninput="this.size=Math.max(this.value.length, 10)" v-model="inputFirstName" placeholder="по имени"
-                @input="searchByInputFirstName">
+              <input class="form-control" type="text" size="10" oninput="this.size=Math.max(this.value.length, 10)" v-model="filterInput.firstName" placeholder="по имени"
+                @input="searchByName('firstName')">
             </div>
           </th>
           <th scope="col-2">
             <div @click="sortBy('surName')" class="table__div_padding table_align">Отчество</div>
             <div>
-              <input class="form-control" type="text" size="10" oninput="this.size=Math.max(this.value.length, 10)" v-model="inputSurName" placeholder="по отчеству"
-                @input="searchByInputSurName">
+              <input class="form-control" type="text" size="10" oninput="this.size=Math.max(this.value.length, 10)" v-model="filterInput.surName" placeholder="по отчеству"
+                @input="searchByName('surName')">
             </div>
           </th>
 
           <th scope="col-2" class="table__input_size">
             <div @click="sortBy('birthday')" class="table__div_padding table_align">Дата рождения</div>
             <div>
-              <input class="form-control color" type="date" v-model="inputBirthday"
-                @input="searchByInputBirthday"
-                :class="{ 'date_color': color }"
+              <input class="form-control" type="date" v-model="filterInput.birthday"
+                @input="searchByDate('birthday')"
                 >
             </div>
           </th>
           <th scope="col-1"  class="table__input_size">
             <div @click="sortBy('age')" class="table__div_padding table_align">Возраст</div>
             <div>
-              <input class="form-control" type="number" v-model="inputAge" @input="searchByInputAge" placeholder="по возрасту">
+              <input class="form-control" type="number" v-model="filterInput.age" @input="searchByNumber('age')" placeholder="по возрасту">
             </div>
           </th>
           <th scope="col-1"></th>
@@ -263,35 +199,4 @@ input::placeholder {
   color:rgba(216, 215, 216, 0.897);
   font-size: 14px;
 }
-
-input[type="date"] {
-  color:black;
-}
-
-
-input[type="date"].date_color {
-  color:rgba(216, 215, 216, 0.897);
-  }
-/* input[type="date"]:not([value="дд.мм.гггг"]) {
-  color:rgba(216, 215, 216, 0.897);
-  } */
-
-
-  
-  /* input[type="date"]:not([value=""]) {
-    color:rgba(216, 215, 216, 0.897);
-  }
-
-  input[type="date"] {
-    background-color: rgba(216, 215, 216, 0.897);
-  /* color:black !important;  */
-/* } */ 
-
-/* input[type="date"] {
-  color:rgba(216, 215, 216, 0.897);
-}
-
-input[type="date"]:focus {
-  color:black;
-} */
 </style>
